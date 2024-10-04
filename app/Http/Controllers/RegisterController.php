eba80577764c0071fa3564859db6178c4012cf24
Ahmad Alkaf <ahmad.alkaf.ahk@gmail.com>
2024-10-04 15:49:37 +0300

    Done authentication (register, login, and logout)

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Laravel\Sanctum\HasApiTokens;
+
+class RegisterController extends Controller
+{
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|string|max:255|unique:' . User::class,
+            'password' => ['required', 'string', Password::defaults()],
+            'birth_date' => 'required|date',
+            'gender' => 'required|string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        $input = $request->all();
+        // $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+        $success['first_name'] =  $user->first_name;
+        $success['last_name'] =  $user->last_name;
+        return $this->sendResponse($success, 'User registered successfully.');
+    }
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|string|max:255',
+            'password' => ['required', Password::defaults()],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = $request->user();
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+            $success['first_name'] =  $user->first_name;
+            $success['last_name'] =  $user->last_name;
+            return $this->sendResponse($success, 'User logged in successfully.');
+        } else {
+            return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        $request->user()->currentAccessToken()->delete();
+        return $this->sendResponse([], "User logged out successfully.");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +17,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::controller(RegisterController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout',[RegisterController::class,'logout']);
+    Route::controller(ProjectController::class)->group(function () {
+        Route::post('project', 'store'); // Create
+        Route::get('project/{id}', 'show'); // Get One
+        Route::get('project', 'index'); // Get All
+        Route::post('project', 'update'); // Update
+        Route::post('project/delete', 'destroy'); // Delete
+    });
+    Route::controller(UserController::class)->group(function () {
+        Route::get('user/{id}', 'show');
+    });
+    // Route::Resource('project', ProjectController::class)
+    //     ->only(['index', 'store', 'show', 'update', 'destroy']);
+});
+
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth:sanctum')->post('/me', function (Request $request) {
+    return $request->user();
+});
+Route::middleware('auth:sanctum')->delete('/me', function (Request $request) {
     return $request->user();
 });
